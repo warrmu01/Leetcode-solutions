33e4a91a781a268adbf69ba059a6f2c065bc90d2
warrmu01 <143742369+warrmu01@users.noreply.github.com>
2024-02-08 23:26:55 -0600

    Add files via upload

---
+++ PalindromeNumber.py
@@ -0,0 +1,32 @@
+
+# Palindrome Number
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# Example 2:
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
+
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+class Solution:
+    def reverse(self, s):
+        str = ""
+        for i in s:
+            str = i + str
+        return str
+    
+    def isPalindrome(self, x: int) -> bool:
+        stringvalue = str(x)
+        reversestringvalue = self.reverse(stringvalue)
+        return stringvalue == reversestringvalue
+
